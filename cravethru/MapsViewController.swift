bdea12a1f8256b9f11c5ec6838ea86c268b8d595
resteybar <resteybar@csumb.edu>
2019-07-07 15:54:22 -0700

    Created Home View and Maps View

---
+++ cravethru/MapsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapsViewController.swift
+//  cravethru
+//
+//  Created by Raymond Esteybar on 7/7/19.
+//  Copyright © 2019 Raymond Esteybar. All rights reserved.
+//
+
+import UIKit
+
+class MapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
