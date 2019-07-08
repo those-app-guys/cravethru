0c9ed29a47fb46f1dc0809a757d9c68a7164b965
resteybar <resteybar@csumb.edu>
2019-07-08 16:57:59 -0700

    Added Bottom Sheet in Maps View, functionality not done

--- cravethru/MapsViewController.swift
+++ cravethru/MapsViewController.swift
@@ -10,12 +10,47 @@ import UIKit
 
 class MapsViewController: UIViewController {
 
+    enum CardState {
+        case expanded
+        case collapsed
+    }
+    
+    var card_view_controller: MapsCardViewController!   // Reference
+    var visual_effect_view: UIVisualEffectView!         // Blur Effect, animate intensity of blur
+    
+    let card_height: CGFloat = 600
+    let card_handle_area_height: CGFloat = 65
+    
+    var card_visible = false
+    var next_state: CardState {
+        return card_visible ? .collapsed : .expanded
+    }
+    
+    // Array of animations
+    var running_animations = [UIViewPropertyAnimator]()
+    // Store progress of animations when it's interrupted
+    var animation_progress_when_interrupted: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup_card()
     }
     
+    func setup_card() {
+        visual_effect_view = UIVisualEffectView()
+        visual_effect_view.frame = self.view.frame
+        self.view.addSubview(visual_effect_view)
+        
+        // Load Card View Controller
+        card_view_controller = MapsCardViewController(nibName: "MapsCard", bundle: nil)
+        self.addChild(card_view_controller)
+        self.view.addSubview(card_view_controller.view)
+        
+        card_view_controller.view.frame = CGRect(x: 0, y: self.view.frame.height - card_handle_area_height, width: self.view.bounds.width, height: card_height)
+        
+        card_view_controller.view.clipsToBounds = true
+    }
 
     /*
     // MARK: - Navigation
