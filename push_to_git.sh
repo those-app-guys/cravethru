db29be1c0fd76ef6edcad648b958db3494cbe481
resteybar <resteybar@csumb.edu>
2019-07-08 17:12:37 -0700

    Added a Github pushing script

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,5 +1,15 @@
 #!/bin/bash  
+
+# To run: 
+# 1. Run: chmod +x push_to_git.sh
+# 2. Run: ./push_to_git.sh "My commit message" my_branch
+#       - Quotes required around for 1st arg.
+
+# Debug
+echo "Commit Message: $1"
+echo "Pushed to: $2"
+
+# Github Pushing
 git add .  
-read -p "Commit description: " desc  
-git commit -m "$desc"  
-git push origin raymond_branch
+git commit -m "$1"
+git push origin $2
