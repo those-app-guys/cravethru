a6133be64319e46a29a86223b05db59c08ecdcaf
erosgonzalez <egonzalez-lopez@csumb.edu>
2019-08-06 16:32:36 -0700

    Tinder card and animation started

--- cravethru/HomeViewController.swift
+++ cravethru/HomeViewController.swift
@@ -1,22 +1,161 @@
 //
-//  ViewController.swift
-//  cravethru
+//  HomeViewController.swift
 //
-//  Created by Raymond Esteybar on 7/7/19.
-//  Copyright © 2019 Raymond Esteybar. All rights reserved.
+//
+//  Created by Eros Gonzalez on 8/6/19.
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+let  MAX_BUFFER_SIZE = 3;
+let  SEPERATOR_DISTANCE = 8;
+let  TOPYAXIS = 75;
 
+class HomeViewController: UIViewController {
+    var db: Firestore!
+    //var prevVC: String!
+    
+    @IBOutlet weak var viewTinderBackGround: UIView!
+    @IBOutlet weak var viewActions: UIView!
+    
+    var currentIndex = 0
+    var currentLoadedCardsArray = [TinderCard]()
+    var allCardsArray = [TinderCard]()
+    var valueArray = ["0","1","2","3","4"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let settings = FirestoreSettings()
+        
+        Firestore.firestore().settings = settings
+        // [END setup]
+        db = Firestore.firestore()
+        
+        viewActions.alpha = 0
+        
         // Do any additional setup after loading the view.
-        print("Start of Crave Thru!")
     }
     
-    private func setupNavigationBarItems() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        view.layoutIfNeeded()
+        loadCardValues()
+    }
+    
+    func loadCardValues() {
+        
+        if valueArray.count > 0 {
+            
+            let capCount = (valueArray.count > MAX_BUFFER_SIZE) ? MAX_BUFFER_SIZE : valueArray.count
+            
+            for (i,value) in valueArray.enumerated() {
+                let newCard = createTinderCard(at: i,value: value)
+                allCardsArray.append(newCard)
+                if i < capCount {
+                    currentLoadedCardsArray.append(newCard)
+                }
+            }
+            
+            for (i,_) in currentLoadedCardsArray.enumerated() {
+                if i > 0 {
+                    viewTinderBackGround.insertSubview(currentLoadedCardsArray[i], belowSubview: currentLoadedCardsArray[i - 1])
+                }else {
+                    viewTinderBackGround.addSubview(currentLoadedCardsArray[i])
+                }
+            }
+            animateCardAfterSwiping()
+            perform(#selector(loadInitialDummyAnimation), with: nil, afterDelay: 1.0)
+            
+        }
+    }
+    
+    @objc func loadInitialDummyAnimation() {
+        
+        let dummyCard = currentLoadedCardsArray.first;
+        dummyCard?.shakeAnimationCard()
+        UIView.animate(withDuration: 1.0, delay: 2.0, options: .curveLinear, animations: {
+            self.viewActions.alpha = 1.0
+        }, completion: nil)
+        //Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.animateEmojiView), userInfo: emojiView, repeats: true)
+    }
+    
+    func createTinderCard(at index: Int , value :String) -> TinderCard {
+        
+        let card = TinderCard(frame: CGRect(x: 0, y: 0, width: viewTinderBackGround.frame.size.width , height: viewTinderBackGround.frame.size.height - 50) ,value : value)
+        card.delegate = self
+        return card
+    }
+    
+    func removeObjectAndAddNewValues() {
+        currentLoadedCardsArray.remove(at: 0)
+        currentIndex = currentIndex + 1
+        
+        if (currentIndex + currentLoadedCardsArray.count) < allCardsArray.count {
+            let card = allCardsArray[currentIndex + currentLoadedCardsArray.count]
+            var frame = card.frame
+            frame.origin.y = CGFloat(MAX_BUFFER_SIZE * SEPERATOR_DISTANCE)
+            card.frame = frame
+            currentLoadedCardsArray.append(card)
+            viewTinderBackGround.insertSubview(currentLoadedCardsArray[MAX_BUFFER_SIZE - 1], belowSubview: currentLoadedCardsArray[MAX_BUFFER_SIZE - 2])
+        }
+        print(currentIndex)
+        animateCardAfterSwiping()
+    }
+    
+    func animateCardAfterSwiping() {
+        for (i,card) in currentLoadedCardsArray.enumerated() {
+            UIView.animate(withDuration: 0.5, animations: {
+                if i == 0 {
+                    card.isUserInteractionEnabled = true
+                }
+                var frame = card.frame
+                frame.origin.y = CGFloat(i * SEPERATOR_DISTANCE)
+                card.frame = frame
+            })
+        }
+    }
+    
+    @IBAction func disLikeButtonAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func LikeButtonAction(_ sender: Any) {
+        
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+extension HomeViewController : TinderCardDelegate{
+    
+    // action called when the card goes to the left.
+    func cardGoesUp(card: TinderCard) {
+        removeObjectAndAddNewValues()
+    }
+    // action called when the card goes to the right.
+    func cardGoesDown(card: TinderCard) {
+        removeObjectAndAddNewValues()
+    }
+    func currentCardStatus(card: TinderCard, distance: CGFloat) {
+        
+        if distance == 0 {
+            //emojiView.rateValue =  2.5
+        }else{
+            //            let value = Float(min(abs(distance/100), 1.0) * 5)
+            //            let sorted = distance > 0  ? 2.5 + (value * 5) / 10  : 2.5 - (value * 5) / 10
+            //emojiView.rateValue =  sorted
+        }
+        
         
     }
 }
