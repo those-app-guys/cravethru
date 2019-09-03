303915642f0ff51e10cef9cfcc5ad1197fa48706
resteybar <resteybar@csumb.edu>
2019-09-03 11:45:03 -0700

    Able to get User Location message and navigate user to Settings

--- cravethru/APICaller/YelpAPI.swift
+++ cravethru/APICaller/YelpAPI.swift
@@ -1,41 +1,48 @@
-////
-////  YelpAPI.swift
-////  cravethru
-////
-////  Created by Raymond Esteybar on 8/4/19.
-////  Copyright © 2019 Raymond Esteybar. All rights reserved.
-////
 //
-//import UIKit
-//import MapKit
-//import Foundation
+//  YelpAPI.swift
+//  cravethru
 //
-//struct Restaurant : Codable {
-//    struct Business : Codable {
-//        let id : String
-//        let name : String
-//    }
-//    
-//    let businesses : [Business]
-//}
+//  Created by Raymond Esteybar on 8/4/19.
+//  Copyright © 2019 Raymond Esteybar. All rights reserved.
 //
-//class YelpAPI {
-//    private static let headers = [
-//        "Authorization": "Bearer hTYF7-rb3s5YvDbH4BtsByLIBaATYCUksKeyNpWAFiDgOfi5EzSrA8YRhqPIUiKUv-p9Ey2E31cHLvCpKbWXBHgT3MKUrr5p7Sbn-ZoeYg91OT-CAZGhedpHC6O3XHYx",
-//        "User-Agent": "PostmanRuntime/7.15.2",
-//        "Accept": "*/*",
-//        "Cache-Control": "no-cache",
-//        "Postman-Token": "84bff9b2-357b-4cc9-b5e6-d7b2ba0505b2,72e2dc29-92b7-4107-b926-ee9b64317e98",
-//        "Host": "api.yelp.com",
-//        "Accept-Encoding": "gzip, deflate",
-//        "Connection": "keep-alive",
-//        "cache-control": "no-cache"
-//    ]
-//    
+
+import UIKit
+import MapKit
+import Foundation
+
+struct YelpRestaurant : Decodable {
+    struct Location : Decodable {
+        let display_address : [String]
+    }
+    
+    struct Business : Decodable {
+        let alias : String          // Used to get us access to the restaurant's photos on Yelp
+        let name : String
+        let location : Location
+    }
+    
+    let businesses : [Business]
+}
+
+class YelpAPI {
+    private static let headers = [
+        "Authorization": "Bearer hTYF7-rb3s5YvDbH4BtsByLIBaATYCUksKeyNpWAFiDgOfi5EzSrA8YRhqPIUiKUv-p9Ey2E31cHLvCpKbWXBHgT3MKUrr5p7Sbn-ZoeYg91OT-CAZGhedpHC6O3XHYx",
+        "User-Agent": "PostmanRuntime/7.15.2",
+        "Accept": "*/*",
+        "Cache-Control": "no-cache",
+        "Postman-Token": "84bff9b2-357b-4cc9-b5e6-d7b2ba0505b2,72e2dc29-92b7-4107-b926-ee9b64317e98",
+        "Host": "api.yelp.com",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "cache-control": "no-cache"
+    ]
+    
 //    class func yelp_business_search(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping (Result<Restaurant, Error>) -> ()) {
 //        print("\nLatitude: \(String(describing: latitude)) | Longitude: \(String(describing: longitude))\n")
 //        
-//        let url_string = "https://api.yelp.com/v3/businesses/search?latitude=\(latitude)&longitude=\(longitude)&categories=restaurants&limit=2"
+//        let request_url = "https://api.foursquare.com/v2/venues/explore"
+//        let parameters = "&ll=\(latitude),\(longitude)&section=\(category)&limit=\(limit)&openNow=\(NSNumber(value: open_now))&sortByDistance=1"
+//        let url_string = request_url + parameters
 //        guard let url = URL(string: url_string) else { return }
 //        
 //        let request = NSMutableURLRequest(url: url,
@@ -60,4 +67,4 @@
 //            }
 //        }.resume()
 //    }
-//}
+}
